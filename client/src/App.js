ad2f248ce8b8710d06308c371f1e0f687b6b9ba6
imkrrish <imkrrish227@gmail.com>
2022-01-20 23:22:31 +0530

    SETTING UP A REACT FRONT-END

---
+++ client/src/App.js
@@ -0,0 +1,39 @@
+import { useEffect } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+
+  useEffect(() => {
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    const accessToken = urlParams.get('access_token');
+    const refreshToken = urlParams.get('refresh_token');
+
+    if (refreshToken) {
+      fetch(`/refresh_token?refresh_token=${refreshToken}`)
+        .then(res => res.json())
+        .then(data => console.log(data))
+        .catch(err => console.error(err));
+    }
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="http://localhost:8888/login"
+        >
+          Login to Spotify
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
--- index.js
+++ index.js
@@ -65,27 +65,23 @@ app.get('/callback', (req, res) => {
     })
         .then(response => {
             if (response.status === 200) {
+                const { access_token, refresh_token } = response.data;
 
-                const { access_token, token_type } = response.data;
+                const queryParams = querystring.stringify({
+                    access_token,
+                    refresh_token,
+                });
 
-                const { refresh_token } = response.data;
-
-                axios.get(`http://localhost:8888/refresh_token?refresh_token=${refresh_token}`)
-                    .then(response => {
-                        res.send(`<pre>${JSON.stringify(response.data, null, 2)}</pre>`);
-                    })
-                    .catch(error => {
-                        res.send(error);
-                    });
+                res.redirect(`http://localhost:3000/?${queryParams}`);
 
             } else {
-                res.send(response);
+                res.redirect(`/?${querystring.stringify({ error: 'invalid_token' })}`);
             }
         })
         .catch(error => {
             res.send(error);
         });
-})
+});
 
 app.get('/refresh_token', (req, res) => {
     const { refresh_token } = req.query;
@@ -112,5 +108,5 @@ app.get('/refresh_token', (req, res) => {
 
 
 app.listen(PORT, () => {
-    console.log(`app is runnig at port ${PORT}`);
+    console.log(`Express app listening at http://localhost:${PORT}`);
 });
